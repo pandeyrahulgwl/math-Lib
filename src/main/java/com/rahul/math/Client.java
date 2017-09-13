6bacbd35fa174471729c6e3b63c0f236a40e88f2
Rahul Pandey <rahulpande@cybage.com>
2017-09-13 09:33:20 +0530

    initial commit

---
+++ src/main/java/com/rahul/math/Client.java
@@ -0,0 +1,7 @@
+package com.rahul.math;
+
+public class Client {
+	public static void main(String[] args) {
+		System.out.println("Addition : "+ Math.add(2, 3));
+	}
+}
---
+++ src/main/java/com/rahul/math/Math.java
@@ -0,0 +1,12 @@
+package com.rahul.math;
+
+public class Math {
+
+	public static int add(Integer num1, Integer num2) {
+		return num1 + num2;
+	}
+
+	public static int subtract(Integer num1, Integer num2) {
+		return num1 - num2;
+	}
+}
